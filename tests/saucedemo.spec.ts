592819909f9c99e02dd1d720716bf3635d1cea3f
dorotkow@gmail.com <dorotkow@gmail.com>
2026-02-25 13:27:30 +0100

    before each added, login switched to login page pom too

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,21 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 
-test('login to saucedemo', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
-
   await expect(page).toHaveTitle(/Swag Labs/);
+});
 
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
+test('login to saucedemo', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.STANDARD_USER || 'standard_user', process.env.PASSWORD || 'secret_sauce');
 
   await expect(page).toHaveURL(/inventory.html/);
 });
 
 test('try to login as locked_out_user', async ({ page }) => {
-  await page.goto('/');
-
   const loginPage = new LoginPage(page);
   await loginPage.login(process.env.LOCKED_OUT_USER || 'locked_out_user', process.env.PASSWORD || 'secret_sauce');
   await expect(loginPage.error).toHaveText('Epic sadface: Sorry, this user has been locked out.');
