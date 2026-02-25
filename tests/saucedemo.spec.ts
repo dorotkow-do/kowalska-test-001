84252515e4751d8f71ecaeb5e8880e718d5beb56
dorotkow@gmail.com <dorotkow@gmail.com>
2026-02-25 13:02:54 +0100

    logn as standard_user, try as locked_out_user

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,8 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('login to saucedemo', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Swag Labs/);
+
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page).toHaveURL(/inventory.html/);
+});
+
+test('try to login as locked_out_user', async ({ page }) => {
+  await page.goto('/');
+
+  await page.locator('[data-test="username"]').fill('locked_out_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Sorry, this user has been locked out.');
 });
